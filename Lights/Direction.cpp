e96ffb1a0fb3c16c742a4ff16bdf6f176d6d5551
esaari1 <esaari1@13d0956a-e706-368b-88ec-5b7e5bac2ff7>
2010-12-21 20:40:27 +0000

    Adding directional lighting.
    git-svn-id: http://ewsraytracer.googlecode.com/svn/trunk@4 13d0956a-e706-368b-88ec-5b7e5bac2ff7

---
+++ Lights/Direction.cpp
@@ -0,0 +1,37 @@
+#include "Direction.h"
+#include "Parser/Hash.h"
+#include "Geometry/GeometryManager.h"
+#include "Geometry/GeometryObject.h"
+
+Direction::Direction() : Light(), ls(1.0), color(), direction() {
+}
+
+Direction::~Direction() {
+}
+
+void Direction::setHash(Hash* hash) {
+   ls = hash->getDouble("radiance");
+   color.set(hash->getValue("color")->getArray());
+   direction.set(hash->getValue("direction")->getArray());
+   direction.normalize();
+}
+
+Vector3D Direction::getLightDirection(const Point3D& hitPoint) {
+   return direction;
+}
+
+bool Direction::inShadow(const Ray& ray, const ShadeRecord& sr) {
+   double t;
+
+   for(GeometryIter it = GeometryManager::instance().begin(); it != GeometryManager::instance().end(); it++) {
+      if((*it)->shadowHit(ray, t)) {
+         return true;
+      }
+   }
+
+   return false;
+}
+
+Color Direction::L(const ShadeRecord& sr) {
+   return color * ls;
+}
---
+++ Lights/Direction.h
@@ -0,0 +1,26 @@
+#ifndef _DIRECTION_H_
+#define _DIRECTION_H_
+
+#include "Light.h"
+#include "Utility/Color.h"
+#include "Math/Vector3D.h"
+
+class Direction : public Light {
+
+public:
+   Direction();
+   ~Direction();
+
+   virtual void setHash(Hash* hash);
+   virtual Vector3D getLightDirection(const Point3D& hitPoint);
+   virtual bool inShadow(const Ray& ray, const ShadeRecord& sr);
+
+   virtual Color L(const ShadeRecord& sr);
+
+private:
+   float ls;
+   Color color;
+   Vector3D direction;
+};
+
+#endif
