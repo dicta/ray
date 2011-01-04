502c1b5749e30585c77ccf5bc44606891f78f570
esaari1 <esaari1@13d0956a-e706-368b-88ec-5b7e5bac2ff7>
2011-01-04 06:02:46 +0000

    Adding base class for compound objects.
    git-svn-id: http://ewsraytracer.googlecode.com/svn/trunk@53 13d0956a-e706-368b-88ec-5b7e5bac2ff7

---
+++ Geometry/Compound/Compound.cpp
@@ -0,0 +1,47 @@
+#include "Compound.h"
+#include <math.h>
+
+bool Compound::hit(const Ray& ray, double& tmin, ShadeRecord& sr) const {
+   if(!bbox.hit(ray)) {
+      return false;
+   }
+   
+   double t;
+   tmin = 1.7 * pow(10.0, 308.0);
+   bool hit = false;
+   Vector3D normal;
+   Point3D localHitPoint;
+   
+   for(GeometryIter it = objects.begin(); it != objects.end(); it++) {
+      if((*it)->hit(ray, t, sr) && (t < tmin)) {
+         hit = true;
+         tmin = t;
+         normal = sr.normal;
+         localHitPoint = sr.localHitPoint;
+      }
+   }
+   
+   if(hit) {
+      sr.t = tmin;
+      sr.normal = normal;
+      sr.localHitPoint = localHitPoint;
+   }
+
+   return hit;
+}
+
+bool Compound::shadowHit(const Ray& ray, double& tmin) const {
+   if(!bbox.hit(ray)) {
+      return false;
+   }
+   
+   tmin = 1.7 * pow(10.0, 308.0);
+   
+   for(GeometryIter it = objects.begin(); it != objects.end(); it++) {
+      if((*it)->shadowHit(ray, tmin)) {
+         return true;
+      }
+   }
+
+   return false;
+}
---
+++ Geometry/Compound/Compound.h
@@ -0,0 +1,29 @@
+#ifndef _COMPOUND_H_
+#define _COMPOUND_H_
+
+#include "Geometry/GeometryObject.h"
+#include "Geometry/BBox.h"
+#include <vector>
+
+using namespace std;
+
+typedef vector<GeometryObject*>::const_iterator GeometryIter;
+
+class Compound : public GeometryObject {
+   
+public:
+   Compound() {}
+   virtual ~Compound() {}
+   
+   virtual bool hit(const Ray& ray, double& tmin, ShadeRecord& sr) const;
+   virtual bool shadowHit(const Ray& ray, double& tmin) const;
+   
+protected:
+   vector<GeometryObject*> objects;
+   BBox bbox;
+   
+   GeometryIter begin() const { return objects.begin(); }
+   GeometryIter end() const { return objects.end(); }
+};
+
+#endif
